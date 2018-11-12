599d3bc3473fc585cf496630e06c733c59e999d1
Unknown <magnusnolsoe@gmail.com>
2018-11-12 12:19:53 +0100

    Changes in command line inputs

--- main.py
+++ main.py
@@ -11,11 +11,17 @@ import argparse
 
 def main():
     parser = argparse.ArgumentParser()
+    group = parser.add_mutually_exclusive_group()
+    
+    group.add_argument("-t", "--train", help="start the training", action="store_true")
+    group.add_argument("-p", "--predict", help="make a prediction", action="store_true")
     
     parser.add_argument("--data_dir", help="relative path to the data directory", default="data")
     parser.add_argument("--dataset", help="name of the dataset")
-    parser.add_argument("-t", "--train", help="start the training", action="store_true")
     parser.add_argument("--checkpoint_dir", help="path to the checkpoint directory", default="checkpoint")
+    
+    # Training parameters
+    parser.add_argument("--epoch", help="number of epochs in training", type=int, default=3)
     
     args = parser.parse_args()
     
@@ -24,6 +30,7 @@ def main():
             raise Exception("Need to specify data directory when training!")
         if not os.path.exists(args.data_dir):
             raise Exception("{} directory does not exist!".format(args.data))
+        
         
         # START TRAINING!
 
