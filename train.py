0ea2c7542d0f4c0f85079937217a22df5f3da6c1
Unknown <magnusnolsoe@gmail.com>
2018-12-26 14:11:45 +0000

    minor error

--- train.py
+++ train.py
@@ -63,10 +63,10 @@ def verbose_train_crf(data_path, model, optimizer, criterion, device, args):
 			
 			mask = build_mask(sequence_lengths).to(device)
 
-			batch_loss = criterion(targets, model_out["f"], model_out["g"],
+			batch_loss = criterion(proteins, model_out["f"], model_out["g"],
                           model_out["alpha"], model_out["beta"], mask)
 			
-			cos_sim = cosine_similarity(model_out["predictions"], targets, mask)
+			cos_sim = cosine_similarity(model_out["p"], targets, mask)
 			
 			err.append(batch_loss.cpu().item())
 			acc.append(cos_sim.cpu().item())
