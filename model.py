99540a9cc9ca098e595e9b1ae62db444f9311969
Unknown <magnusnolsoe@gmail.com>
2019-01-03 01:50:10 +0000

    refactoring and running save

--- model.py
+++ model.py
@@ -1,80 +1,16 @@
 import torch.nn as nn
 import torch
-import crf; import sys
+import crf
 
 from torch.autograd import Variable 
 from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
-from torch import relu, sigmoid
+from torch import relu, sigmoid, softmax
 
-class Net(nn.Module):
+class BiRNN_CRF(nn.Module):
     def __init__(self, device, num_embeddings=21, embedding_dim=32,
                  rnn_hidden_size=100, rnn_layers=2, rnn_dropout=0.3, bi_dir=True,
-                 linear_out=20, linear_dropout=0.5):
-        super(Net, self).__init__()
-        
-        self.device = device
-
-        self.embedding = nn.Embedding(num_embeddings, embedding_dim)
-        
-        self.rnn_hidden_size = rnn_hidden_size
-        self.num_layers = rnn_layers
-        self.directions= 2 if bi_dir else 1
-        self.linear_units = linear_out
-        rnn_dropout = rnn_dropout if rnn_layers > 1 else 0
-        
-        self.LSTM = nn.LSTM(embedding_dim, hidden_size=rnn_hidden_size, num_layers=rnn_layers,
-                           batch_first=True, dropout=rnn_dropout, bidirectional=bi_dir)
-        
-        self.dropout = nn.Dropout(linear_dropout)
-        
-        self.Linear = nn.Linear(self.directions*rnn_hidden_size, linear_out, bias=True)
-
-        self.activation = nn.Softmax(dim=1)
-
-    def init_hidden(self, batch_size):
-        h0 = torch.randn(self.num_layers*self.directions, batch_size, self.rnn_hidden_size).to(self.device)
-        c0 = torch.randn(self.num_layers*self.directions, batch_size, self.rnn_hidden_size).to(self.device)
-        
-        h0 = Variable(h0)
-        c0 = Variable(c0)
-
-        return (h0, c0)
-
-        
-    def forward(self, batch, seq_lengths):
-        
-        batch_size = len(batch)
-
-        embedded_batch = self.embedding(batch)
-        
-        # Pack sequence
-        packed = pack_padded_sequence(embedded_batch, seq_lengths, batch_first=True)
-        
-        hidden_state_params = self.init_hidden(batch_size)
-        rnn_out, (h_n, c_n) = self.LSTM(packed, hidden_state_params)
-        
-        # Unpack sequence
-        x, _ = pad_packed_sequence(rnn_out, batch_first=True, padding_value=0)
-        
-        n = x.size()[1]
-        
-        x = x.contiguous()
-        x = x.view(-1, x.shape[2])
-    
-        x = self.dropout(x)
-        x = self.Linear(x)        
-        x = self.activation(x)
-        
-        x = x.view(batch_size, n, self.linear_units)
-        
-        return x
-
-
-class CRF_Net(nn.Module):
-    def __init__(self, device, num_embeddings=21, embedding_dim=32,
-                 rnn_hidden_size=100, rnn_layers=2, rnn_dropout=0.3, bi_dir=True,
-                 linear_units=200, classes=20, linear_dropout=0.5):
-        super(CRF_Net, self).__init__()
+                 hidden_linear_units=200, classes=20, linear_dropout=0.5, crf_on=False):
+        super(BiRNN_CRF, self).__init__()
         
         self.device = device
 
@@ -84,7 +20,8 @@ class CRF_Net(nn.Module):
         self.num_layers = rnn_layers
         self.directions= 2 if bi_dir else 1
         self.classes = classes
-        self.linear_units = linear_units
+        self.linear_units = hidden_linear_units
+        self.crf_on = crf_on
         rnn_dropout = rnn_dropout if rnn_layers > 1 else 0
         
         self.LSTM = nn.LSTM(embedding_dim, hidden_size=rnn_hidden_size, num_layers=rnn_layers,
@@ -92,11 +29,13 @@ class CRF_Net(nn.Module):
         
         self.dropout = nn.Dropout(linear_dropout)
         
-        self.linear = nn.Linear(self.directions*rnn_hidden_size, linear_units)
+        self.linear_hidden = nn.Linear(self.directions*rnn_hidden_size, hidden_linear_units)
         
-        self.f = nn.Linear(linear_units, classes, bias=True)
-        self.g = nn.Linear(2*linear_units, classes**2, bias=True)
-
+        self.f = nn.Linear(hidden_linear_units, classes, bias=True)
+        
+        if crf_on:
+            self.g = nn.Linear(2*hidden_linear_units, classes**2, bias=True)
+        
 
     def init_hidden(self, batch_size):
         h0 = torch.randn(self.num_layers*self.directions, batch_size, self.rnn_hidden_size).to(self.device)
@@ -127,25 +66,30 @@ class CRF_Net(nn.Module):
         h = h.contiguous().view(-1, h.size()[2])
         
         x = self.dropout(h)
-        x = relu(self.linear(x))
+        x = relu(self.linear_hidden(x))
+        x = self.dropout(x)
         
-        g_in = []
-        for batch in x.view(batch_size, n, self.linear_units):
-            for i in range(n-1):
-                g_in.append(torch.cat((batch[i], batch[i+1])))
-        g_in = torch.stack(g_in)
-
         f = self.f(x)
-        g = self.g(g_in)
-
         f = f.view(batch_size, n, self.classes)
-        g = g.view(batch_size, n-1, self.classes, self.classes)
         
-        nu_alp = crf.forward_pass(f, g, self.device)
-        nu_bet = crf.backward_pass(f, g, self.device)
-        
-        x = crf.log_marginal(nu_alp, nu_bet)
-        x = torch.exp(x)
-        
-        return {"p": x, "f": f, "g": g,
-                "alpha": nu_alp, "beta": nu_bet}
+        if self.crf_on:
+            
+            g_in = []
+            for batch in x.view(batch_size, n, self.linear_units):
+                for i in range(n-1):
+                    g_in.append(torch.cat((batch[i], batch[i+1])))
+            g_in = torch.stack(g_in)
+    
+            g = self.g(g_in)
+            g = g.view(batch_size, n-1, self.classes, self.classes)
+            
+            nu_alp = crf.forward_pass(f, g, self.device)
+            nu_bet = crf.backward_pass(f, g, self.device)
+            
+            log_pred = crf.log_marginal(nu_alp, nu_bet)
+            prediction = torch.exp(log_pred)
+            
+        else:
+            prediction = softmax(f, dim=2)
+            
+        return prediction
--- train.py
+++ train.py
@@ -1,220 +1,182 @@
+import os
+import pickle
+import torch
+
 from tqdm import tqdm
 from data import DataLoader, DataIterator
 from utils import build_mask, cosine_similarity
 
-def verbose_train_crf(data_path, model, optimizer, criterion, device, args):
-	data_loader = DataLoader(data_path, args.verbose)
+def train(data_path, model, optimizer, criterion, device, logger, args):
+    data_loader = DataLoader(data_path, args.verbose)
 
-	X, y, seq = data_loader.run_pipeline(args.split_rate)
-	
-	train_iter = DataIterator(X[0], y[0], seq[0], batch_size=args.batch_size)
-	test_iter = DataIterator(X[1], y[1], seq[1], batch_size=args.batch_size)
-	
-	train_err, test_err = [], []
-	train_acc, test_acc = [], []
-	
-	print(model)
-	
-	for epoch in range(args.epoch):
-		
-		print("Epoch: {} / {}".format(epoch+1, args.epoch))
+    X, y, seq = data_loader.run_pipeline(args.split_rate)
 
-		### TRAIN LOOP ###
-		err, acc = [], []
-		model.train()
-		for proteins, sequence_lengths, targets in tqdm(train_iter, ascii=False, desc="Training", total=int(len(X[0]) / args.batch_size), unit="batch"):
+    train_iter = DataIterator(X[0], y[0], seq[0], batch_size=args.batch_size)
+    test_iter = DataIterator(X[1], y[1], seq[1], batch_size=args.batch_size)
 
-			inputs = proteins.to(device)
-			seq_lens = sequence_lengths.to(device)
-			targets = targets.to(device)
-			
-			model_out = model(inputs, seq_lens)
-              
+    train_err, test_err = [], []
+    train_acc, test_acc = [], []
 
-			mask = build_mask(sequence_lengths).to(device)
+    logger.info(model)
+
+    for epoch in range(args.epoch):
+
+        logger.info("Epoch: {} / {}".format(epoch+1, args.epoch))
+
+        ### TRAIN LOOP ###
+        err = []
+        acc = []
+        model.train()
+        for proteins, sequence_lengths, targets in (tqdm(train_iter, ascii=False, desc="Training", total=int(len(X[0]) / args.batch_size), unit="batch") if args.verbose else train_iter):
+
+            inputs = proteins.to(device)
+            seq_lens = sequence_lengths.to(device)
+            targets = targets.to(device)
+
+            predictions = model(inputs, seq_lens)
+
+            mask = build_mask(sequence_lengths).to(device)
+
+            optimizer.zero_grad()
+            batch_loss = criterion(predictions, targets, mask)
+            batch_loss.backward()
+            optimizer.step()
+
+            cos_sim = cosine_similarity(predictions, targets, mask)
+
+            err.append(batch_loss.cpu().item())
+            acc.append(cos_sim.cpu().item())
+
+        epoch_trainig_error = sum(err) / len(err)
+        epoch_training_accuracy = sum(acc) / len(acc)
+        train_err.append(epoch_trainig_error)
+        train_acc.append(epoch_training_accuracy)
+
+
+        ### TEST LOOP ###
+        err = []
+        acc = []
+        model.eval()
+        for proteins, sequence_lengths, targets in (tqdm(test_iter, ascii=False, desc="Testing", total=int(len(X[1]) / args.batch_size), unit="batch") if args.verbose else test_iter):
+
+            inputs = proteins.to(device)
+            seq_lens = sequence_lengths.to(device)
+            targets = targets.to(device)
+
+            predictions = model(inputs, seq_lens)      
+
+            mask = build_mask(sequence_lengths).to(device)
+
+            batch_loss = criterion(predictions, targets, mask)
+
+            cos_sim = cosine_similarity(predictions, targets, mask)
+
+            err.append(batch_loss.cpu().item())
+            acc.append(cos_sim.cpu().item())
+
+        epoch_test_error = sum(err) / len(err)
+        epoch_test_accuracy = sum(acc) / len(acc)
+        test_err.append(epoch_test_error)
+        train_acc.append(epoch_training_accuracy)
+
+        logger.info("Training error: {0:.4f},\tTest error: {1:.4f}\t\tTraining accuracy: {2:.4f}\tTest accuracy: {3:.4f}".format(epoch_trainig_error, epoch_test_error, epoch_training_accuracy, epoch_test_accuracy))
+
+    return (train_err, test_err), (train_acc, test_acc)
+
+def train_running_save(data_path, model, optimizer, criterion, device, logger, args, step=10):
+    
+    if not os.path.exists("results"):
+        os.mkdir("results")
+    if not os.path.exists(args.checkpoint_dir):
+        os.mkdir(args.checkpoint_dir)
+    
+    data_loader = DataLoader(data_path, args.verbose)
+
+    X, y, seq = data_loader.run_pipeline(args.split_rate)
+
+    train_iter = DataIterator(X[0], y[0], seq[0], batch_size=args.batch_size)
+    test_iter = DataIterator(X[1], y[1], seq[1], batch_size=args.batch_size)
+
+    train_err, test_err = [], []
+    train_acc, test_acc = [], []
+
+    logger.info(model)
+
+    for epoch in range(args.epoch):
+
+        logger.info("Epoch: {} / {}".format(epoch+1, args.epoch))
+
+        ### TRAIN LOOP ###
+        err = []
+        acc = []
+        model.train()
+        for proteins, sequence_lengths, targets in (tqdm(train_iter, ascii=False, desc="Training", total=int(len(X[0]) / args.batch_size), unit="batch") if args.verbose else train_iter):
+
+            inputs = proteins.to(device)
+            seq_lens = sequence_lengths.to(device)
+            targets = targets.to(device)
+
+            predictions = model(inputs, seq_lens)
+
+            mask = build_mask(sequence_lengths).to(device)
+
+            optimizer.zero_grad()
+            batch_loss = criterion(predictions, targets, mask)
+            batch_loss.backward()
+            optimizer.step()
+
+            cos_sim = cosine_similarity(predictions, targets, mask)
+
+            err.append(batch_loss.cpu().item())
+            acc.append(cos_sim.cpu().item())
+
+        epoch_trainig_error = sum(err) / len(err)
+        epoch_training_accuracy = sum(acc) / len(acc)
+        train_err.append(epoch_trainig_error)
+        train_acc.append(epoch_training_accuracy)
+
+
+        ### TEST LOOP ###
+        err = []
+        acc = []
+        model.eval()
+        for proteins, sequence_lengths, targets in (tqdm(test_iter, ascii=False, desc="Testing", total=int(len(X[1]) / args.batch_size), unit="batch") if args.verbose else test_iter):
+
+            inputs = proteins.to(device)
+            seq_lens = sequence_lengths.to(device)
+            targets = targets.to(device)
+
+            predictions = model(inputs, seq_lens)      
+
+            mask = build_mask(sequence_lengths).to(device)
+
+            batch_loss = criterion(predictions, targets, mask)
+
+            cos_sim = cosine_similarity(predictions, targets, mask)
+
+            err.append(batch_loss.cpu().item())
+            acc.append(cos_sim.cpu().item())
+
+        epoch_test_error = sum(err) / len(err)
+        epoch_test_accuracy = sum(acc) / len(acc)
+        test_err.append(epoch_test_error)
+        train_acc.append(epoch_training_accuracy)
+
+        logger.info("Training error: {0:.4f},\tTest error: {1:.4f}\t\tTraining accuracy: {2:.4f}\tTest accuracy: {3:.4f}".format(epoch_trainig_error, epoch_test_error, epoch_training_accuracy, epoch_test_accuracy))
         
-			optimizer.zero_grad(); batch_loss = criterion(model_out["p"], targets, mask)
-			#batch_loss = criterion(proteins, model_out["f"], model_out["g"],
-                          #model_out["alpha"], model_out["beta"], mask)
-			batch_loss.backward()
-			optimizer.step()
+        if epoch % step == 0:
             
-			cos_sim = cosine_similarity(model_out["p"], targets, mask)
-			
-			err.append(batch_loss.cpu().item())
-			acc.append(cos_sim.cpu().item())
+            logger.info("Saving checkpoint")
+            
+            performance_path = os.path.join("results", "{}-epoch{}.pk".format(args.results_name.split(".")[0], epoch)) # temporary name
+            checkpoint_name = "{}-epoch{}.pt".format(args.checkpoint_name.split(".")[0], epoch) # temporary name
+            results = (train_err, test_err), (train_acc, test_acc)
+            with open(performance_path, "wb") as file:
+                pickle.dump(results, file)
+            torch.save({
+                "epoch": args.epoch,
+                "model_state_dict": model.cpu().state_dict(),
+                "optimizer_state_dict": optimizer.state_dict() 
+                }, os.path.join(args.checkpoint_dir, checkpoint_name))  
 
-		epoch_trainig_error = sum(err) / len(err)
-		epoch_training_accuracy = sum(acc) / len(acc)
-		train_err.append(epoch_trainig_error)
-		train_acc.append(epoch_training_accuracy)
-
-	
-		### TEST LOOP ###
-		err, acc = [], []
-		model.eval()
-		for proteins, sequence_lengths, targets in tqdm(test_iter, ascii=False, desc="Testing", total=int(len(X[1]) / args.batch_size), unit="batch"):
-
-			inputs = proteins.to(device)
-			seq_lens = sequence_lengths.to(device)
-			targets = targets.to(device)
-
-			model_out = model(inputs, seq_lens)      
-			
-			mask = build_mask(sequence_lengths).to(device); batch_loss = criterion(model_out["p"], targets, mask)
-
-			#batch_loss = criterion(proteins, model_out["f"], model_out["g"],
-                         #model_out["alpha"], model_out["beta"], mask)
-			cos_sim = cosine_similarity(model_out["p"], targets, mask)
-			
-			err.append(batch_loss.cpu().item())
-			acc.append(cos_sim.cpu().item())
-
-		epoch_test_error = sum(err) / len(err)
-		epoch_test_accuracy = sum(acc) / len(acc)
-		test_err.append(epoch_test_error); test_acc.append(epoch_test_accuracy)
-		
-		print("Training error: {0:.4f},\tTest error: {1:.4f}\t\tTraining accuracy: {2:.4f}\tTest accuracy: {3:.4f}".format(epoch_trainig_error, epoch_test_error, epoch_training_accuracy, epoch_test_accuracy))
-			
-	return (train_err, test_err), (train_acc, test_acc)
-
-
-
-def verbose_train(data_path, model, optimizer, criterion, device, args):
-	data_loader = DataLoader(data_path, args.verbose)
-
-	X, y, seq = data_loader.run_pipeline(args.split_rate)
-	
-	train_iter = DataIterator(X[0], y[0], seq[0], batch_size=args.batch_size)
-	test_iter = DataIterator(X[1], y[1], seq[1], batch_size=args.batch_size)
-	
-	train_err, test_err = [], []
-	train_acc, test_acc = [], []
-	
-	print(model)
-	
-	for epoch in range(args.epoch):
-		
-		print("Epoch: {} / {}".format(epoch+1, args.epoch))
-
-		### TRAIN LOOP ###
-		err = []
-		acc = []
-		model.train()
-		for proteins, sequence_lengths, targets in tqdm(train_iter, ascii=False, desc="Training", total=int(len(X[0]) / args.batch_size), unit="batch"):
-
-			inputs = proteins.to(device)
-			seq_lens = sequence_lengths.to(device)
-			targets = targets.to(device)
-			
-			predictions = model(inputs, seq_lens)
-			
-			mask = build_mask(sequence_lengths).to(device)
-
-			optimizer.zero_grad()
-			batch_loss = criterion(predictions, targets, mask)
-			batch_loss.backward()
-			optimizer.step()
-
-			cos_sim = cosine_similarity(predictions, targets, mask)
-			
-			err.append(batch_loss.cpu().item())
-			acc.append(cos_sim.cpu().item())
-
-		epoch_trainig_error = sum(err) / len(err)
-		epoch_training_accuracy = sum(acc) / len(acc)
-		train_err.append(epoch_trainig_error)
-		train_acc.append(epoch_training_accuracy)
-
-	
-		### TEST LOOP ###
-		err = []
-		acc = []
-		model.eval()
-		for proteins, sequence_lengths, targets in tqdm(test_iter, ascii=False, desc="Testing", total=int(len(X[1]) / args.batch_size), unit="batch"):
-
-			inputs = proteins.to(device)
-			seq_lens = sequence_lengths.to(device)
-			targets = targets.to(device)
-
-			predictions = model(inputs, seq_lens)      
-			
-			mask = build_mask(sequence_lengths).to(device)
-
-			batch_loss = criterion(predictions, targets, mask)
-			
-			cos_sim = cosine_similarity(predictions, targets, mask)
-			
-			err.append(batch_loss.cpu().item())
-			acc.append(cos_sim.cpu().item())
-
-		epoch_test_error = sum(err) / len(err)
-		epoch_test_accuracy = sum(acc) / len(acc)
-		test_err.append(epoch_test_error)
-		
-		print("Training error: {0:.4f},\tTest error: {1:.4f}\t\tTraining accuracy: {2:.4f}\tTest accuracy: {3:.4f}".format(epoch_trainig_error, epoch_test_error, epoch_training_accuracy, epoch_test_accuracy))
-			
-	return (train_err, test_err)
-
-
-def train(data_path, net, optimizer, criterion, device, args):
-	
-	data_loader = DataLoader(data_path, args.verbose)
-
-	X, y, seq = data_loader.run_pipeline(args.split_rate)
-	
-	train_iter = DataIterator(X[0], y[0], seq[0], batch_size=args.batch_size)
-	test_iter = DataIterator(X[1], y[1], seq[1], batch_size=args.batch_size)
-	
-	train_err, test_err = [], []
-	
-	for epoch in range(args.epoch):
-
-		### TRAIN LOOP ###
-		err = []
-		net.train()
-		for proteins, sequence_lengths, targets in train_iter:
-
-			inputs = proteins.to(device)
-			seq_lens = sequence_lengths.to(device)
-			targets = targets.to(device)
-			
-			predictions = net(inputs, seq_lens)                
-			
-			mask = build_mask(sequence_lengths).to(device)
-
-			optimizer.zero_grad()
-			batch_loss = criterion(predictions, targets, mask)
-			batch_loss.backward()
-			optimizer.step()
-
-			err.append(batch_loss.cpu().item())
-
-		epoch_trainig_error = sum(err) / len(err)
-		train_err.append(epoch_trainig_error)
-
-	
-		### TEST LOOP ###
-		err = []
-		net.eval()
-		for proteins, sequence_lengths, targets in test_iter:
-
-			inputs = proteins.to(device)
-			seq_lens = sequence_lengths.to(device)
-			targets = targets.to(device)
-
-			predictions = net(inputs, seq_lens)      
-			
-			mask = build_mask(sequence_lengths).to(device)
-
-			batch_loss = criterion(predictions, targets, mask)
-
-			err.append(batch_loss.cpu().item())
-
-		epoch_test_error = sum(err) / len(err)
-		test_err.append(epoch_test_error)
-			
-	return (train_err, test_err)
-# plt.plot(train_err, 'ro-', label="train error")
-# plt.plot(test_err, 'bo-', label="test error")
-# plt.show()
+    return (train_err, test_err), (train_acc, test_acc)
